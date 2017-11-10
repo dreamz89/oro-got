909cf534c21c8ab3b27eb63a50b7bd8c029ed379
Jocelyn Tan <jocelyntan89@hotmail.com>
2017-11-10 23:36:09 +0800

    formatted the requirements component

--- src/homepage/Requirements.js
+++ src/homepage/Requirements.js
@@ -6,12 +6,22 @@ import Skills from '../images/horseriding.svg'
 export default class Requirements extends Component {
   render () {
     return (
-      <div className='req'>
-        <div className='container'>
-          <h1>Key Requirements</h1>
+      <div className='container'>
+        <h1>Requirements</h1>
+        <div className='req'>
           <img src={Hair} alt='Long hair man' />
+          <h2>Long Hair</h2>
+          <p>For men, the longer hair the better, for a medieval look.</p>
+        </div>
+        <div className='req'>
           <img src={Discretion} alt='discretion' />
+          <h2>Discretion</h2>
+          <p>Do not approach actors, unless they approach you first. Do not write on social media or tell friends and family about the filming.</p>
+        </div>
+        <div className='req'>
           <img src={Skills} alt='skills' />
+          <h2>Skills</h2>
+          <p>Medieval skills like horse riding/ archery / wood chopping / sword fighting are good to have</p>
         </div>
       </div>
     )
