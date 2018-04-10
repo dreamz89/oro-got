eaa39934da06f952d93cd5c1ac539cbb4b63b3f1
Jocelyn Tan <jocelyntan89@hotmail.com>
2018-04-10 13:45:36 +0800

    changed section bg color

--- src/homepage/Requirements.js
+++ src/homepage/Requirements.js
@@ -6,23 +6,25 @@ import Skills from '../images/horseriding.svg'
 export default class Requirements extends Component {
   render () {
     return (
-      <div className='container'>
-        <h1>Requirements</h1>
-        <p>Besides being reliable and flexible, people whose looks fit the medieval scenes would be great</p>
-        <div className='col req'>
-          <img src={Hair} alt='Long hair man' />
-          <h3>Long Hair</h3>
-          <p>For men, the longer hair the better, for a medieval look.</p>
-        </div>
-        <div className='col req'>
-          <img src={Discretion} alt='discretion' />
-          <h3>Discretion</h3>
-          <p>Do not approach actors, unless they approach you first. Do not write on social media or tell friends and family about the filming.</p>
-        </div>
-        <div className='col req'>
-          <img src={Skills} alt='skills' />
-          <h3>Skills</h3>
-          <p>Medieval skills like horse riding/ archery / wood chopping / sword fighting are good to have</p>
+      <div className='reqbg'>
+        <div className='container'>
+          <h1>Requirements</h1>
+          <p>Besides being reliable and flexible, people whose looks fit the medieval scenes would be great</p>
+          <div className='col req'>
+            <img src={Hair} alt='Long hair man' />
+            <h3>Long Hair</h3>
+            <p>For men, the longer hair the better, for a medieval look.</p>
+          </div>
+          <div className='col req'>
+            <img src={Discretion} alt='discretion' />
+            <h3>Discretion</h3>
+            <p>Do not approach actors, unless they approach you first. Do not write on social media or tell friends and family about the filming.</p>
+          </div>
+          <div className='col req'>
+            <img src={Skills} alt='skills' />
+            <h3>Skills</h3>
+            <p>Medieval skills like horse riding/ archery / wood chopping / sword fighting are good to have</p>
+          </div>
         </div>
       </div>
     )
